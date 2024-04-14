e61b658f8bf613ceb3be09a63ec3ef9dd7d1e330
Valentine199 <jeszenoi.balint@gmail.com>
2024-04-14 21:20:20 +0200

    methode self documentations

--- ros2_course/turtlesim_controller.py
+++ ros2_course/turtlesim_controller.py
@@ -23,6 +23,20 @@ class TurtlesimController(Node):
         self.pose = msg
 
     def go_straight(self, speed, distance):
+        """
+        Move the robot straight at a specified speed for a given distance.
+
+        Parameters:
+            speed (float): The linear speed of the robot in meters per second. Positive value for forward motion, negative for backward.
+            distance (float): The distance the robot should travel in meters. Positive value for forward motion, negative for backward.
+
+        Notes:
+            - This method blocks until the robot reaches the desired destination.
+            - The robot's pose (position and orientation) must be available before calling this method.
+            - The robot must have a functional ROS 2 environment with a Twist message publisher set up.
+            - The method uses a proportional control (P-controller) to adjust the robot's speed based on the distance error.
+
+        """
         # Wait for position to be received
         loop_rate = self.create_rate(100, self.get_clock()) # Hz
         while self.pose is None and rclpy.ok():
@@ -84,6 +98,21 @@ class TurtlesimController(Node):
         #self.get_logger().info('Arrived to destination.')
 
     def turn(self, omega, angle):
+        """
+        Turn the robot at a specified angular velocity to achieve a desired angle.
+
+        Parameters:
+            omega (float): The angular velocity of the robot in radians per second. Positive value for counterclockwise rotation, negative for clockwise.
+            angle (float): The angle by which the robot should turn in degrees. Positive value for counterclockwise rotation, negative for clockwise.
+
+        Notes:
+            - This method blocks until the robot reaches the desired orientation.
+            - The robot's pose (position and orientation) must be available before calling this method.
+            - The robot must have a functional ROS 2 environment with a Twist message publisher set up.
+            - The method uses a proportional control (P-controller) to adjust the robot's angular velocity based on the angle error.
+            - The robot will stop when it reaches within 0.015 degrees of the target angle.
+
+        """
         # Wait for position to be received
         loop_rate = self.create_rate(75, self.get_clock()) # Hz
         while self.pose is None and rclpy.ok():
@@ -152,6 +181,24 @@ class TurtlesimController(Node):
         #self.get_logger().info('Arrived to destination.')
         
     def _get_front_coordinates(self, tx, ty, theta, distance):
+        """
+        Calculate the coordinates in front of the robot based on its orientation.
+
+        Parameters:
+            tx (float): The x-coordinate of the robot's current position.
+            ty (float): The y-coordinate of the robot's current position.
+            theta (float): The orientation angle of the robot in radians.
+            distance (float): The distance from the current position to the front position.
+
+        Returns:
+            front_x (float): The x-coordinate of the position in front of the robot.
+            front_y (float): The y-coordinate of the position in front of the robot.
+
+        Notes:
+            - This method calculates the coordinates in front of the robot based on its current position and orientation.
+            - The distance parameter determines how far in front of the robot the coordinates will be calculated.
+            - The orientation angle theta is assumed to be in radians.
+        """
         # Calculate the x, y offset based on the turtle's orientation
         dx = distance * math.cos(theta)
         dy = distance * math.sin(theta)
@@ -163,6 +210,21 @@ class TurtlesimController(Node):
         return front_x, front_y
 
     def _is_destination_in_front(self, dest_x, dest_y):
+        """
+        Determine if the destination point is in front of the robot.
+
+        Parameters:
+            dest_x (float): The x-coordinate of the destination point.
+            dest_y (float): The y-coordinate of the destination point.
+
+        Returns:
+            in_front (bool): True if the destination point is in front of the robot, False otherwise.
+
+        Notes:
+            - This method calculates the angle between the robot's orientation and the vector pointing to the destination point.
+            - If the absolute difference between this angle and the robot's orientation is less than 90 degrees, the destination is considered to be in front of the robot.
+            - The angle is normalized to be between -180 and 180 degrees.
+        """
         # Calculate the angle between turtle orientation and vector to destination
         angle_to_dest = math.atan2(dest_y- self.pose.y, dest_x - self.pose.x)
         angle_diff = math.degrees(abs(angle_to_dest - self.pose.theta))
@@ -177,6 +239,20 @@ class TurtlesimController(Node):
             return False
     
     def _normalise_angle(self, angle):
+        """
+        Normalize the angle to be within the range [0, 360) degrees.
+
+        Parameters:
+            angle (float): The angle to be normalized.
+
+        Returns:
+            normalized_angle (float): The normalized angle within the range [0, 360) degrees.
+
+        Notes:
+            - This method ensures that the angle is within the range [0, 360) degrees by adjusting it accordingly.
+            - Angles less than 0 are adjusted by adding 360 degrees.
+            - Angles greater than or equal to 360 are adjusted by subtracting 360 degrees.
+        """
         if angle < 0:
             angle += 360
         elif angle > 360:
@@ -219,6 +295,16 @@ class FractalTree():
         self.turtle.turn(self.omega, angle)
     
     def setup_turtle(self, offset):
+        """
+        Set up the turtle's initial position and speed.
+
+        Parameters:
+            offset (float): The offset distance from the starting position.
+
+        Notes:
+            - This method positions the turtle to the left and backward by 90 degrees and the specified offset distance.
+            - If the turtle's speed is not set or set to a non-positive value, it is defaulted to 20 units.
+        """
         self._left(90)
         self._backward(offset)
 
@@ -226,6 +312,19 @@ class FractalTree():
             self.speed = 20
     
     def draw_tree(self, i: int):
+        """
+        Recursively draw a tree-like structure using turtle graphics.
+
+        Parameters:
+            i (int): The current length of the branch.
+
+        Notes:
+            - This method recursively draws a tree-like structure using turtle graphics.
+            - The parameter 'i' represents the current length of the branch.
+            - The recursion stops when the length of the branch 'i' falls below 0.5 units.
+            - At each step, the method draws a branch of length 'i' and then recursively calls itself to draw the sub-branches.
+            - The angles for branching are set to 30 degrees to the left and 60 degrees to the right.
+        """
         #print(i)
         if i < 0.5:
             return
@@ -254,7 +353,7 @@ def main(args=None):
     v = 2.5
     tc.setup_turtle(v)
     tc.draw_tree(v)
-    
+
     # Destroy the node explicitly
     # (optional - otherwise it will be done automatically
     # when the garbage collector destroys the node object)
